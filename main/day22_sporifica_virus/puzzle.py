c3ec6a78f2b874f7a924c9b96410c53236a0576c
Zimmer-Bentin, Karl-Dieter (GOD) <karl-dieter.zimmer-bentin@god.de>
2023-04-19 12:47:41 +0200

    day 22 part 1 and 2 done

--- main/day22_sporifica_virus/puzzle.py
+++ main/day22_sporifica_virus/puzzle.py
@@ -28,6 +28,13 @@ TURN_LEFT: dict[str, str] = {
     "right": "up",
 }
 
+REVERT: dict[str, str] = {
+    "up": "down",
+    "left": "right",
+    "down": "up",
+    "right": "left",
+}
+
 
 def parse(puzzle: str) -> set[Vector]:
     with open(puzzle) as file:
@@ -61,18 +68,53 @@ class Carrier:
             self.position.col + DIRECTION[self.heading].col,
         )
 
+    def smart_burst(self, weakened: set[Vector], infected: set[Vector], flagged: set[Vector]) -> None:
+        pos = self.position
+        if pos in weakened:
+            # weakened node: does not change direction
+            weakened.remove(pos)
+            infected.add(pos)
+            self.infection_count += 1
+        elif pos in infected:
+            self.heading = TURN_RIGHT[self.heading]
+            infected.remove(pos)
+            flagged.add(pos)
+        elif pos in flagged:
+            self.heading = REVERT[self.heading]
+            flagged.remove(pos)
+        else:
+            self.heading = TURN_LEFT[self.heading]
+            weakened.add(pos)
+        self.position = Vector(
+            self.position.row + DIRECTION[self.heading].row,
+            self.position.col + DIRECTION[self.heading].col,
+        )
 
-def part1(puzzle: str):
+
+def part1(puzzle: str) -> int:
     infected = parse(puzzle)
     carrier = Carrier(Vector(0, 0), "up")
-    for _ in range(10000):
+    for _ in range(10_000):
         carrier.burst(infected)
-    print(carrier.infection_count)
+    return carrier.infection_count
+
+
+def part2(puzzle: str, bursts: int) -> int:
+    infected = parse(puzzle)
+    weakened: set[Vector] = set()
+    flagged: set[Vector] = set()
+    carrier = Carrier(Vector(0, 0), "up")
+    for _ in range(bursts):
+        carrier.smart_burst(weakened, infected, flagged)
+    return carrier.infection_count
 
 
 def main():
-    part1("test.txt")
-    part1("puzzle.txt")
+    print("part 1 test:", "OK" if part1("test.txt") == 5587 else "Fail!")
+    print("part 2 test 1:", "OK" if part2("test.txt", 100) == 26 else "Fail!")
+    print("part 2 test 2:", "OK" if part2("test.txt", 10_000_000) == 2511944 else "Fail!")
+    print("part 1 solution =", part1("puzzle.txt"), "<- shall be 5538")
+    print("part 2 solution =", part2("puzzle.txt", 10_000_000), "<- shall be 2511090")
 
 
 if __name__ == '__main__':
